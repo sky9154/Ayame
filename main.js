705f6cef3b0ae79251c80722adff1664015dbf3e
oF <sky0932039612@gmail.com>
2021-09-28 13:57:50 +0800

    修改程式碼

--- main.js
+++ main.js
@@ -1,142 +1,117 @@
-const fs = require("fs");    // fs module ，用來操作實體檔案，可以同步或非同步存取檔案系統操作
-const http = require("http");    // http module 提供了許多 function 以及 class 来搭建 HTTP 伺服器
-const path = require("path");    // path module 用來處理與轉換路徑的方法與屬性
+const fs = require("fs")    // fs module ，用來操作實體檔案，可以同步或非同步存取檔案系統操作
+const http = require("http")    // http module 提供了許多 function 以及 class 来搭建 HTTP 伺服器
+const path = require("path")    // path module 用來處理與轉換路徑的方法與屬性
 const port = process.env.PORT || 5000;     // 設定 Node.js 使用 heroku 的 port 或是 本地的 port 為 5000 
-const dir = "./images"    // 設置圖檔位置  
+const dir = "./images"    // 設置圖片位置  
 
-function getStat(path) {
+// 讀取圖片路徑
+function getImages(path) {
     return new Promise((resolve, reject) => {
         fs.stat(path, (err, stats) => {
-            if(err){
-                resolve(false);
-            }else{
-                resolve(stats);
-            }
+            if(err) resolve(false)
+            else resolve(stats)
         })
     })
 }
 
+// 創建路徑
 function mkdir(dir){
     return new Promise((resolve, reject) => {
         fs.mkdir(dir, err => {
-            if(err){
-                resolve(false);
-            }else{
-                resolve(true);
-            }
+            if(err) resolve(false)
+            else resolve(true)
         })
     })
 }
-async function dirExists(dir){
-    let isExists = await getStat(dir);
-    if(isExists && isExists.isDirectory()){
-        return true;
-    }else if(isExists){
-        return false;
-    }
-    let tempDir = path.parse(dir).dir;
-    let status = await dirExists(tempDir);
-    let mkdirStatus;
-    if(status){
-        mkdirStatus = await mkdir(dir);
-    }
-    return mkdirStatus;
+
+// 判斷路徑是否存在，不存在則創建
+async function dirExists(dir) {
+    let filePath = await getImages(dir)    // 取得路徑
+
+    if(filePath && filePath.isDirectory()) return true;
+    else if(filePath) return false
+
+    let status = await dirExists(path.parse(dir).dir)   // 遞迴判斷，如果上層資料夾不存在，則會在此處繼續循環執行，直到目錄存在
+    let mkdirStatus
+
+    if(status) mkdirStatus = await mkdir(dir);
+
+    return mkdirStatus
 }
 
-async function fn(){
-    await dirExists('./images');
-}
-fn();
+dirExists("./images")
 
-function readdir (res){
-    return new Promise((resolve,reject) =>{
-        fs.readdir(res,(err,data)=>{
-            if(err){
-                reject(err)
-            }
+// 讀取圖片資料夾內的文件和子資料夾
+function readdir(res) {
+    return new Promise((resolve, reject) => {
+        fs.readdir(res, (err, data) => {
+            if(err) reject(err)
             resolve(data)
         })
     })
 }
 
-function filestat (res){
-    return new Promise((resolve,reject) =>{
-        fs.stat(res,(err,data) =>{
-            if(err){
-                reject(err)
-            }
+// 讀取圖片屬性
+function filestat(res) {
+    return new Promise((resolve, reject) => {
+        fs.stat(res,(err,data) => {
+            if(err) reject(err)
             resolve(data)
         })
     })
 }
 
-function readfile (res){
-    return new Promise((resolve,reject)=>{
-        fs.readFile(res,(err,data)=>{
-            if(err){
-                reject(err)
-            }
+// 讀取圖片內容
+function readfile(res) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(res, (err, data) => {
+            if(err) reject(err)
             resolve(data)
         })
     })
 }
 
-function filename(picDir){
-    return new Promise((resolve,reject)=>{
+// 讀取圖片名稱
+function filename(picDir) {
+    return new Promise((resolve, reject) => {
         readdir(picDir)
-            .then(data=>{
+        .then(data => {
             let paths = []
             let name
-            data.map((name,index)=>{
+
+            data.map((name, index) => {
                 let tmpPath = path.join(picDir, name)
-                filestat(tmpPath).then((stats)=>{
-                    if(!stats.isDirectory() && path.extname(tmpPath).toUpperCase() === ".JPG"){                  
-                        paths.push(tmpPath)
-                    }
-                    if(index+1 === data.length){
-                        resolve(paths)
-                    }
+
+                filestat(tmpPath).then(stats => {
+                    if(!stats.isDirectory() && path.extname(tmpPath).toUpperCase() === ".JPG") paths.push(tmpPath)
+                    if(index+1 === data.length) resolve(paths)
                 })
-                .catch((err)=>{
-                    reject(err)
-                })      
+                .catch(err => reject(err))      
             })    
         })
-        .catch((err)=>{
-            reject(err)
-        })
+        .catch(err => reject(err))
     })
 }
 
-function rd(n){ 
-    return Math.floor(Math.random() * (n+1))
-}
-
+// 隨機取得圖片路徑
 function rdpic(){
-    return new Promise((resolve,reject)=>{
+    return new Promise((resolve, reject) => {
         filename(dir)
-            .then(data=>{
-            let n = rd(data.length-1)
+            .then(data => {
+            let n = Math.floor(Math.random() * (data.length))   // 取得 0 到 n 的隨機數字
             resolve(data[n])
         })
     })
 }
 
+// 啟動伺服器
 http.createServer(server).listen(port)
 
 function server (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.writeHead(200, {"Content-Type": "text/plain"})
     rdpic()
-    .then(data =>{
-        return readfile(data)
-    })
-    .then(data =>{
-        return data
-    })
-    .then(data=>{
-        res.end(data)
-    })
-    .catch(err => {
-        throw err
-    })
+    .then(data => {return readfile(data)})
+    .then(data => {return data})
+    .then(data => {res.end(data)})
+    .catch(err => {throw err});
 }
